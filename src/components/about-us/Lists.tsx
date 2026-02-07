443f26935c38b5a19b07679ea417b9da5ee4e25a
Fajri Bagaskara Budi <fajribagaskara69@gmail.com>
2026-02-07 20:00:47 +0700

    feat: added about us page

--- src/components/about-us/Lists.tsx
+++ src/components/about-us/Lists.tsx
@@ -8,9 +8,9 @@ export default function Lists({ icon="", text="" }: ListsProps ) {
         <div className='flex gap-2 md:gap-3.5 items-center'>
             <div className="bg-[#3B82F6]/20 rounded-3xl">
                 <img src={`/about-us/${icon}.svg`} alt="" 
-                className={`size-4 ${icon==="student" ? "size-5 md:size-7 m-2 sm:m-[5px]" : "m-2.5"}`}/>
+                className={`size-4 ${icon==="student" ? "size-5 md:size-7 m-2 sm:size-6 sm:m-[6.5px] md:m-[5px]" : "m-2.5"}`}/>
             </div>
-            <p className='font-nunito font-medium text-[13px] md:text-[16px]'>{text}</p>
+            <p className='font-nunito font-medium text-[13px] md:text-[16px] 2xl:text-[20px]'>{text}</p>
         </div>
     ) 
 }
