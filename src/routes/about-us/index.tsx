ec96eea3da43888ee984f856e80c96c47eff8fb3
Fajri Bagaskara Budi <fajribagaskara69@gmail.com>
2026-02-07 16:49:16 +0700

    feat: added not found page

--- src/routes/about-us/index.tsx
+++ src/routes/about-us/index.tsx
@@ -1,5 +1,6 @@
 import { createFileRoute } from '@tanstack/react-router'
 import Footer from '@/components/Footer'
+import PillInfo from '@/components/PillInfo'
 
 export const Route = createFileRoute('/about-us/')({
   component: RouteComponent,
@@ -7,9 +8,26 @@ export const Route = createFileRoute('/about-us/')({
 
 function RouteComponent() {
   return (
-      <div>
-        <div className='h-screen'>Hello "/about-us"!</div>
-        <Footer />
+    <>
+      <div className='flex flex-col justify-center text-center items-center relative overflow-y-scroll scrollbar-hide h-screen'>
+    
+          <div className='flex flex-col items-center h-[20vh] justify-center'>
+            <PillInfo text='ABOUT US' showIcon={false} icon=''/>
+    
+            <div className='flex flex-col leading-9.5 mt-4'>
+              <h1 className='font-nunito font-bold text-[40px] sm:text-[50px] 2xl:text-[64px]'>Siapa Kita?</h1>
+    
+              <p className='font-ibm font-bold text-[20px] bg-linear-to-r bg-clip-text text-transparent from-[#2563EB] to-[#10B981]'>Apa itu AIChE?</p>
+            </div>
+          </div>
+
+          <div className='flex flex-col w-screen h-screen px-7 relative'>
+            <p className='font-ibm font-medium text-[12px]'>AIChE SC ITENAS is a premier student body dedicated to bridging the gap between academia and industry through sustainable engineering practices. We are a community of innovators, thinkers, and future leaders.</p>
+          </div>
+    
       </div>
+    
+      <Footer />
+    </>
   )
 }
